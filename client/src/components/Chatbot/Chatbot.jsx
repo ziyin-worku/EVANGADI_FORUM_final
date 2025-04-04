0dc8964023672ebbd795eb963feed456c2c8cf6f
ziyin-worku <ziyinworku@gmail.com>
2025-04-05 00:06:28 +0300

    final

--- client/src/components/Chatbot/Chatbot.jsx
+++ client/src/components/Chatbot/Chatbot.jsx
@@ -1,30 +1,3 @@
-// import React, { useEffect } from "react";
-
-// const Chatbot = () => {
-//   useEffect(() => {
-//     const script = document.createElement("script");
-//     script.src = "https://www.chatbase.co/embed.min.js";
-//     script.id = "ATyPv_1ZlunmN7lkaV6N4";
-//     script.async = true;
-//     document.body.appendChild(script);
-
-//     return () => {
-//       document.body.removeChild(script);
-//     };
-//   }, []);
-
-//   return (
-//     <div>
-//       <div id="chatbase-container"></div>{" "}
-//       {/* Ensure a div is available for the bot */}
-//     </div>
-//   );
-// };
-
-// export default Chatbot;
-
-
-
 import React, { useEffect } from "react";
 
 const Chatbot = () => {
